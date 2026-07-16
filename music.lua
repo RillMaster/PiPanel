8128edee7be89f71eb9d184a430964bf70ec1b63
RillMaster <94992948+RillMaster@users.noreply.github.com>
2026-07-16 19:24:58 +0200

    Update music.lua

--- music.lua
+++ music.lua
@@ -1,31 +1,7 @@
-local user = "RillMaster"
-local pass = "Kylian120909%40%21"
-local server = "http://91.173.80.213:49153"
-
-local url = server ..
-"/rest/getSongs.view" ..
-"?u="..user..
-"&p="..pass..
-"&v=1.16.1"..
-"&c=ComputerCraft"..
-"&f=json"
-
-local response = http.get(url)
+local response, err = http.get(url)
 
 if not response then
-    print("Erreur connexion Navidrome")
+    print("Erreur HTTP :")
+    print(err)
     return
-end
-
-local data = textutils.unserializeJSON(response.readAll())
-
-local songs = data["subsonic-response"].songs.song
-
-if not songs then
-    print("Aucune musique trouvée")
-    return
-end
-
-for i, song in ipairs(songs) do
-    print(i..". "..song.title.." - "..song.artist)
 end
