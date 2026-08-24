928e34f75fd9c5ad236d520a52870c99b02c6622
RillMaster <kylian@rillmaster.fr>
2026-08-25 00:19:08 +0200

    chore: bump version for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.rillmaster.pipanel"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.5.2"
+        versionCode = 15
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
